6adaf3e95940bd0722c62f0c476d7c2dcc94fca3
Nofal71 <hafiznofal76@gmail.com>
2023-10-16 00:16:17 +0500

    Adding Numbers

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World----------------------");
     System.out.println("Nofal hassaan");
-    System.out.print("My First Repository");
+    System.out.println("My First Repository");
+    System.out.println(1+5);
+    System.out.println(2*8);
   }
 }
