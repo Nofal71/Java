d864fd62f7262661b65c0cf74afc500e69336241
Nofal71 <hafiznofal76@gmail.com>
2023-10-16 00:37:44 +0500

    Adding Variables

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World----------------------");
+    System.out.println("Hello World");
     System.out.println("Nofal hassaan");
     System.out.println("My First Repository");
+    //Println is used to endline , whilw print() line not ending
+    // Without "" to display numbers:
     System.out.println(1+5);
     System.out.println(2*8);
+    System.out.println(6/10);
+    //Variables
+    String name = "Nofal";
+    int mynum = 12;
+    char myLetter = 'D';
+    System.out.println(name);
+    System.out.println(mynum);
+    System.out.println(myLetter);
+    // combining variables
+
+    String firstname = "Nofal";
+    String Lastname = " Hassaan";
+    String Fullname = firstname + Lastname;
+    System.out.println(Fullname);
+
   }
 }
