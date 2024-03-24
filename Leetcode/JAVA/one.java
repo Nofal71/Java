0a4c97222d8762c199a98c5736c1842cf350c098
Nofal71 <hafiznofal76@gmail.com>
2024-03-24 11:07:35 +0500

    #1 array Concatination

---
+++ Leetcode/JAVA/one.java
@@ -0,0 +1,23 @@
+
+
+
+public class one {
+   
+    public static int[] concatination(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] =  ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int[] result = concatination(nums); 
+       
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;;
 
-public class Exercises {
+public class App {
     public static void main (String[] args)
 
    { 
@@ -99,7 +99,4 @@ Test Data:
 
 
 
-}
-
-}
 
