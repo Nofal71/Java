a3fad9ea8243385e4d5b0a793097a74d8212acb5
Nofal71 <hafiznofal76@gmail.com>
2024-03-25 10:59:41 +0500

    #2 Permutation

---
+++ Leetcode/JAVA/two.java
@@ -0,0 +1,28 @@
+public class two {
+    
+    public static void Zero(int[] array , int size){
+   
+        int[] ans = new int[size];
+
+        for(int i = 0 ; i < size ; i++){
+
+        ans[i] = array[array[i]];
+        if(array[i] == 0){
+            ans[i] = array[0];
+        }
+        }
+
+        for(int x : ans){
+            System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+    
+    
+    int[] nums = {1, 0 ,2 ,3 , 0};
+    int size  = nums.length;
+     Zero(nums , size);
+
+    }
+}
