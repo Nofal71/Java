8e1013c44dea7ae9b3907c6e2207a3853069f2f9
Nofal71 <hafiznofal76@gmail.com>
2024-03-26 15:13:40 +0500

    Good Pairs

--- Leetcode/JAVA/two.java
+++ Leetcode/JAVA/two.java
@@ -1,28 +1,23 @@
 public class two {
-    
-    public static void Zero(int[] array , int size){
-   
+
+    public static void Zero(int[] array, int size) {
+
         int[] ans = new int[size];
-
-        for(int i = 0 ; i < size ; i++){
-
-        ans[i] = array[array[i]];
-        if(array[i] == 0){
-            ans[i] = array[0];
+        for (int i = 0; i < size; i++) {
+            ans[i] = array[array[i]];
+            if (array[i] == 0) {
+                ans[i] = array[0];
+            }
         }
-        }
-
-        for(int x : ans){
+        for (int x : ans) {
             System.out.print(x + " ");
         }
     }
 
     public static void main(String[] args) {
-    
-    
-    int[] nums = {1, 0 ,2 ,3 , 0};
-    int size  = nums.length;
-     Zero(nums , size);
+        int[] nums = { 1, 0, 2, 3, 0 };
+        int size = nums.length;
+        Zero(nums, size);
 
     }
 }
