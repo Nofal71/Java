d604f80870a23d85116528af88963111027f7696
Nofal71 <hafiznofal76@gmail.com>
2024-03-28 17:57:02 +0500

    Final Value of Variable After Performing Operations

---
+++ Leetcode/JAVA/four.java
@@ -0,0 +1,30 @@
+public class four {
+
+    public static int operations(String[] operations) {
+        int X = 0;
+
+        for (int j = 0; j < operations.length; j++) {
+            switch (operations[j]) {
+                case "X++":
+                case "++X":
+                    X++;
+                    break;
+                case "X--":
+                case "--X":
+                    X--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return X;
+    }
+
+    public static void main(String[] args) {
+
+        String[] operations = { "++X", "++X", "X++" };
+        System.out.println(operations(operations));
+
+    }
+}
--- Leetcode/JAVA/three.java
+++ Leetcode/JAVA/three.java
@@ -1,26 +1,22 @@
 public class three {
- 
-    public static void goodPairs(int array[]){
-    
-        for(int i = 0 ; i < array.length ; i++){
-        for(int j = 0 ; j < array.length ; j++){
-        if(array[i] == array[j] && i<j){
-            System.out.print(i+" , "+j);
-            System.out.println();
-        }
-        }
-        }
 
+    public static void goodPairs(int array[]) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j] && i < j) {
+                    System.out.print(i + " , " + j);
+                    System.out.println();
+                }
+            }
+        }
 
     }
 
-    public static void main(String[] args) 
-{
+    public static void main(String[] args) {
 
- int nums[] = {1,2,3,1,1,3};
- goodPairs(nums);
+        int nums[] = { 1, 2, 3, 1, 1, 3 };
+        goodPairs(nums);
 
-
-
-}
+    }
 }
