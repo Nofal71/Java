f797fe9b2484cfe166bdd8f86e5172e0d50d2443
Nofal71 <hafiznofal76@gmail.com>
2024-04-03 14:07:07 +0500

    Number of Employees Who Met the Target

---
+++ Leetcode/JAVA/Eight.java
@@ -0,0 +1,23 @@
+public class Eight{
+    
+        public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+    
+        for (int i = 0; i < accounts.length; i++) {
+            int wealth = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                wealth += accounts[i][j];
+            }
+            if (wealth > maxWealth) {
+                maxWealth = wealth;
+            }
+        }
+    
+        return maxWealth;
+        }
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Leetcode/JAVA/nine.java
@@ -0,0 +1,18 @@
+public class nine {
+    public static int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int numOfEmployes = 0;
+
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] >= target) {
+                numOfEmployes++;
+            }
+        }
+        return numOfEmployes;
+    }
+
+    public static void main(String[] args) {
+        int[] hours = { 0, 1, 2, 3, 4 };
+        int target = 2;
+        System.out.println("Number of Employes who met Target Hours = "+numberOfEmployeesWhoMetTarget(hours, target));
+    }
+}
