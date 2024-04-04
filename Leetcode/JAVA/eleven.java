c810967d0ce894830968a477e1f83c1df16d8656
Nofal71 <hafiznofal76@gmail.com>
2024-04-05 00:20:58 +0500

    Count Pairs Whose Sum is Less than Target

---
+++ Leetcode/JAVA/eleven.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class eleven {
+
+    public static int countPairs(List<Integer> nums, int target) {
+        int pairs = 0;
+        int n = nums.size();
+        // size() is used to get length of list
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (nums.get(i) + nums.get(j) < target) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(-1, 1, 2, 3, 1);
+        int target = 2;
+
+        System.out.println(countPairs(nums, target));
+    }
+}
