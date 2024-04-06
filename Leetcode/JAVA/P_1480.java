9e4b57a39e479ebea1c0361b4c5abd32a021fc55
Nofal71 <hafiznofal76@gmail.com>
2024-04-06 14:17:30 +0500

    updating P_1480

--- Leetcode/JAVA/P_1480.java
+++ Leetcode/JAVA/P_1480.java
@@ -11,12 +11,17 @@ public class P_1480 {
         return sumArray;
     }
 
+    public static void runArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " , ");
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 4 };
         int[] result = runningSum(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(result[i] + " , ");
-        }
+        runArray(result);
+
     }
 
 }
