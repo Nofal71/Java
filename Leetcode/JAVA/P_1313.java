da0f2ffb70ef3916f975b64cce9cc0c9812c5d4e
Nofal71 <hafiznofal76@gmail.com>
2024-04-17 08:54:00 -0700

    Decompress Run-Length Encoded List

---
+++ Leetcode/JAVA/P_1313.java
@@ -0,0 +1,37 @@
+public class P_1313 {
+    public static int[] decompressRLElist(int[] nums) {
+        int size = 0;
+
+        for (int i = 0; i < nums.length; i+=2) {
+            size += nums[i];
+        }
+
+        int[] decompressRLElist = new int[size];
+
+        int index = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            int freq = nums[i];
+            int val = nums[i + 1];
+
+            for (int j = 0; j < freq; j++) {
+                decompressRLElist[index] = val;
+                index++;
+            }
+
+        }
+
+        return decompressRLElist;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = { 1, 1, 2, 3 };
+        int[] result = decompressRLElist(nums);
+        for (int x : result) {
+            System.out.println(x);
+        }
+
+    }
+
+}
