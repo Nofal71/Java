4a93c4c4b47df1ec1195b7d0cf0c7e8aca694518
Nofal71 <hafiznofal76@gmail.com>
2024-05-03 09:55:41 -0700

    new logics for P_1684

--- Leetcode/JAVA/P_1684.java
+++ Leetcode/JAVA/P_1684.java
@@ -2,47 +2,20 @@ public class P_1684 {
     public static int countConsistentStrings(String allowed, String[] words) {
         int ans = 0;
 
-        // for (int i = 0; i < allowed.length() - 1; i++) { // Loop for allowed
-        //     for (int j = 0; j < words.length; j++) { // Loop for words array index
-
-        //         Boolean temp1 = false;
-        //         Boolean temp2 = false;
-
-        //         for (int k = 0; k < words[j].length(); k++) { // Loop for indexed word length
-
-        //                 temp1 = false;
-        //                 temp2 = false;
-        //                 break;
-        //             }
-
-        //                 temp1 = true;
-        //                 temp2 = true;
-        //             }
-
-        //         }
-        //             ans++;
-        //         }
-        //     }
-        // }
-
-
         for (int i = 0; i < words.length; i++) {
-            boolean consistent = true; // Assume the word is consistent until proven otherwise
+            boolean consistent = true; 
 
             for (int j = 0; j < words[i].length(); j++) {
                 if (allowed.indexOf(words[i].charAt(j)) == -1) {
-                    consistent = false; // If any character is not allowed, mark the word as inconsistent
-                    break; // No need to continue checking this word
+                    consistent = false; 
+                    break; 
                 }
             }
 
             if (consistent) {
-                ans++; // If the word is consistent, increment the count
+                ans++; 
             }
         }
-
-
-
 
 
         return ans;
