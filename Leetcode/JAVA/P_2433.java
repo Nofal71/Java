26f01d0d34e3352adb829934df5c6ccfef0400c4
Nofal71 <hafiznofal76@gmail.com>
2024-05-09 09:39:53 -0700

    XOR , medium array problem

--- Leetcode/JAVA/P_2433.java
+++ Leetcode/JAVA/P_2433.java
@@ -2,33 +2,23 @@ public class P_2433 {
     public static int[] findArray(int[] pref) {
 
         int[] arr = new int[pref.length];
+        arr[0] = pref[0];
 
-        return arr;
-    }
-
-    public static int[] toBinary(int Number) {
-
-        int numBits = (int) (Math.log(Number) / Math.log(2)) + 1;
-        int[] remainder = new int[numBits];
-
-        for (int i = 0; i < numBits; i++) {
-
-            remainder[i] = Number % 2;
-
-            if (Number == 0) {
-                break;
-            } else {
-                Number /= 2;
-            }
+        for (int i = 1; i < pref.length; i++) {
+            arr[i] = pref[i] ^ pref[i - 1];
         }
 
-        return remainder;
+        return arr;
+
     }
 
     public static void main(String[] args) {
-        int[] result = toBinary(13);
-        for (int i = 0; i < 4; i++) {
-            System.out.println(result[i]);
+        int[] arr = { 5, 2, 0, 3, 1 };
+        int[] ans = findArray(arr);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
         }
+
     }
+
 }
