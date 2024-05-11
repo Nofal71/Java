05760f30040e797b151ccfe830bcc88132dbb1b3
Nofal71 <hafiznofal76@gmail.com>
2024-05-11 06:27:58 -0700

    #4 on Leetcode , Hard , Median of Two arrays , Easy solution :

---
+++ Leetcode/JAVA/P_4.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class P_4 {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length = nums1.length + nums2.length;
+
+        int[] array = new int[length];
+        int ind = 0;
+        for (int i = 0; i <= length - 1; i++) {
+            if (i < nums1.length) {
+                array[i] = nums1[i];
+            } else {
+                array[i] = nums2[ind];
+                ind++;
+            }
+        }
+
+        Arrays.sort(array);
+        int temp1 = (array.length / 2) - 1;
+        int temp2 = (array.length / 2);
+        double ans = array[temp2] ;
+
+        if(array.length % 2 == 0){
+             ans = (array[temp1] + array[temp2]) / 2.0 ;
+        }
+        return ans ;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2 };
+        int[] nums2 = { 3 , 4 };
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+}
