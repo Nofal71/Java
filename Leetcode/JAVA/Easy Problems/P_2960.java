7ac354c62ef0226954a0d92898ba47deab74038f
Nofal71 <hafiznofal76@gmail.com>
2024-05-25 11:54:06 +0500

    Count of Battery Tested devices

---
+++ Leetcode/JAVA/Easy Problems/P_2960.java
@@ -0,0 +1,22 @@
+public class P_2960 {
+    public static int countTestedDevices(int[] batteryPercentages) {
+        int count = 0;
+
+        for (int i = 0; i < batteryPercentages.length; i++) {
+            if (batteryPercentages[i] > 0) {
+                count++;
+                for (int j = i + 1; j < batteryPercentages.length; j++) {
+                    batteryPercentages[j] -= 1;
+                }
+            }
+        }
+
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        int[] batteryPercentages = { 1, 1, 2, 1, 3 };
+        System.out.println(countTestedDevices(batteryPercentages));
+    }
+}
