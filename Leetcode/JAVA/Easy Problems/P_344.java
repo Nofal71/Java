0f53e0d774fe5c71ec7fef7baf4257fd2ff2b763
Nofal71 <hafiznofal76@gmail.com>
2024-06-02 11:57:54 +0500

    Reverse Orignal String , in-place

---
+++ Leetcode/JAVA/Easy Problems/P_344.java
@@ -0,0 +1,24 @@
+public class P_344 {
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {  // Change condition to left < right
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            right--;
+            left++;
+        }
+
+        for (char x : s) {
+            System.out.print(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = { 'H', 'a', 'n', 'n', 'a' , 'h'};
+        reverseString(s);
+    }
+}
