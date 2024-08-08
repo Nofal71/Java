1203b17508da64d9f3d5bd24a1666bacdfd5b370
Nofal71 <hafiznofal76@gmail.com>
2024-08-08 13:56:58 +0500

    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/1348578524/ , DONE this with Binary

--- Leetcode (DSA)/ARRAY/JavaScript/Medium Problems/P_34.js
+++ Leetcode (DSA)/ARRAY/JavaScript/Medium Problems/P_34.js
@@ -1,23 +1,64 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var searchRange = function (nums, target) {
+//   if (!nums.includes(target)) return [-1, -1];
+//   let ans = [],
+//     freq = 0,
+//     index = nums.indexOf(target);
+//   ans.push(index);
+//   for (let i = index; i < nums.length; i++) {
+//     if (nums[i] != target) break;
+//     freq++;
+//   }
+//   ans.push(index - 1 + freq);
+
+//   return ans;
+// };
+
+// let nums = [3, 3, 3],
+//   target = 3;
+// console.log(searchRange(nums, target));
+
+
+
+
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  if (!nums.includes(target)) return [-1, -1];
-  let ans = [],
-    freq = 0,
-    index = nums.indexOf(target);
-  ans.push(index);
-  for (let i = index; i < nums.length; i++) {
-    if (nums[i] != target) break;
-    freq++;
-  }
-  ans.push(index - 1 + freq);
+  let start = searchRange_1(nums, target , true)
+  let end = searchRange_1(nums, target , false)
+  return [ start ,end]
 
-  return ans;
 };
 
-let nums = [3, 3, 3],
-  target = 3;
+var searchRange_1 = function (nums, target, firstIndex) {
+  let ans = -1
+  let start = 0, end = nums.length - 1;
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) / 2)
+    if (nums[mid] > target) {
+      end = mid - 1
+    } else if (nums[mid] < target) {
+      start = mid + 1
+    } else {
+      ans = mid
+      if(firstIndex){
+        end = mid-1
+      }else{
+        start = mid+1
+      }
+    }
+  }
+  return ans
+};
+
+
+let nums = [5, 7, 7, 8, 8, 10], target = 8
 console.log(searchRange(nums, target));
